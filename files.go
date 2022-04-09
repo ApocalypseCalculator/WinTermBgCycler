e837b96be8722a7a239faac17f150fd24560f1f1
Patrick Lin <apocalypsecalculator@gmail.com>
2022-04-09 16:40:25 -0400

    create files if they do not exist

---
+++ files.go
@@ -0,0 +1,19 @@
+package main
+
+import "os"
+
+func checkFiles() {
+	//write default config
+	if _, err := os.Stat("config.json"); err != nil {
+		os.WriteFile("config.json", []byte("{\n\t\"wintermsettings\": \"\",\n\t\"picspathfile\": \"backgrounds/pics.txt\",\n\t\"picsfolder\": \"backgrounds\",\n\t\"interval\": 60\n}"), 0644)
+	}
+	settings := initialize()
+	if settings.PicsPathFile == "backgrounds/pics.txt" {
+		if _, err := os.Stat("backgrounds"); err != nil {
+			os.Mkdir("backgrounds", 0644)
+		}
+		if _, err := os.Stat(settings.PicsPathFile); err != nil {
+			os.WriteFile(settings.PicsPathFile, []byte("& You can either place your pictures inside this folder or append the file path of the image to this file\n"), 0644)
+		}
+	}
+}
